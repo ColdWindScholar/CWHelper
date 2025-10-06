dfa3ad11284183601a7133f3d96834aba959fabd
ColdWindScholar <3590361911@qq.com>
2025-10-06 19:37:50 +0800

    init

--- src/mtdunpk.py
+++ src/mtdunpk.py
@@ -1,15 +1,17 @@
 file_path = "MTDs/"
-from .utils import call
+from utils import call
 import json
-
-
+from jefferson.jffs2 import extract_jffs2
+from pathlib import Path
 def unpack(mtd, type_, image_name):
     print("=====================================")
     if type_ == "squashfs":
         call(["unsquashfs", "-d", f"{mtd}_unpacked", mtd])
         print(f"已解包squashfs分区：{mtd}")
     elif type_ == 'jffs2':
-        call(['jefferson', mtd, "-d", f"{file_path}/{image_name}_unpacked"])
+        file = Path(mtd)
+        dest = Path(f"{file_path}/{image_name}_unpacked")
+        extract_jffs2(file, dest, verbose=False)
         print("已解包jffs2分区：", mtd)
     else:
         print("已忽略", type_, "分区：", mtd)
@@ -22,3 +24,5 @@ def main():
     for part in partitions:
         target_file = f"{file_path}/{part['file']}"
         unpack(target_file, part['fst'], part['file'])
+
+main()
