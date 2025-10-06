c9f798ee8814623807e54c3b58e47ababa6eb392
ColdWindScholar <3590361911@qq.com>
2025-10-06 22:37:21 +0800

    Fix Mtd unpack

--- src/fillend.py
+++ src/fillend.py
@@ -8,7 +8,8 @@ def fill(file_path, desired_size, fill_char = None):
     current_size = os.path.getsize(file_path)
     bytes_to_fill = desired_size - current_size
     if bytes_to_fill > 0:
-        with open(file_path, 'r+b') as file:
+        with open(file_path, 'a+b') as file:
+            file.seek(0, 2)
             while bytes_to_fill > 0:
                 chunk_size = min(bytes_to_fill, 16)
                 file.write(fill_char * chunk_size)
--- src/mtdjoin.py
+++ src/mtdjoin.py
@@ -7,7 +7,7 @@ def main():
     file_path = "MTDs/"
     if not os.path.exists(file_path + "/partitions.json") and not os.path.exists(file_path + "/mtd0"):
         print("无法识别工程文件夹，请检查：", file_path)
-        return
+        return 1
     file_out = file_path + "/full_new.bin"
     if os.path.exists(file_path + "/partitions.json"):
         print("检测到新版mtd集合格式")
@@ -26,6 +26,10 @@ def main():
                 if size < part['size']:
                     print("分区文件", src, "太小，自动填充0xFF")
                     fill(src, part['size'])
+                    if os.path.getsize(file_out) == part['size']:
+                        print("填充成功！")
+                    else:
+                        print("填充失败:",os.path.getsize(file_out), part['size'])
                 elif size > part['size']:
                     print("检测到严重错误：")
                     print("分区文件", src, "太大，请检查")
