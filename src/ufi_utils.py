f9a1e0c3f08e095c9e13059158d93778149f83cb
ColdWindScholar <3590361911@qq.com>
2025-10-12 00:09:32 +0800

    add at command

--- src/ufi_utils.py
+++ src/ufi_utils.py
@@ -2,12 +2,17 @@ import os
 if os.name == 'nt':
     import wmi
 
-def devcon(str_list:list):
-    c = wmi.WMI()
-    devices = c.Win32_PnPEntity()
-    for device in devices:
-        device_str = device.name + devices.deviceid
-        for i in str_list:
-            if i in device_str:
-                return True
-    return False
+
+    def devcon(str_list: list):
+        c = wmi.WMI()
+        devices = c.Win32_PnPEntity()
+        for device in devices:
+            device_str = device.name + devices.deviceid
+            for i in str_list:
+                if i in device_str:
+                    return True
+        return False
+elif os.name == 'posix':
+    def devcon(str_list: list):
+        print(f"Are you sure {str_list} is your device?")
+        return input('Y/N: ').lower() == 'y'
