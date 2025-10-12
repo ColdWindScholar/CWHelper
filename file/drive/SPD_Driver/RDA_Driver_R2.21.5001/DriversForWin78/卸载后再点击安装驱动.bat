52b75e3b5b8a38cb7ecd264fb69c334ac4f50836
ColdWindScholar <3590361911@qq.com>
2025-10-12 10:23:16 +0800

    init

--- file/drive/SPD_Driver/RDA_Driver_R2.21.5001/DriversForWin78/卸载后再点击安装驱动.bat
+++
@@ -1,9 +0,0 @@
-if "%PROCESSOR_ARCHITECTURE%"=="x86" goto x86
-if "%PROCESSOR_ARCHITECTURE%"=="AMD64" goto x64
-exit
-:x64
-start DPInst64
-exit
- 
-:x86
-start DPInst32
--- file/drive/SPD_Driver/RDA_Driver_R2.21.5001/DriversForWin78/首先请点击卸载驱动.bat
+++
@@ -1,9 +0,0 @@
-if "%PROCESSOR_ARCHITECTURE%"=="x86" goto x86
-if "%PROCESSOR_ARCHITECTURE%"=="AMD64" goto x64
-exit
-:x64
-start DriverUninstall64
-exit
- 
-:x86
-start DriverUninstall32
